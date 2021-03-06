b14cf8e1702648d947c25be7e02b165ee2975916
Vincent <415578597@qq.com>
2021-03-06 15:26:29 +1000

    :fire: delete unused link

--- src/App.js
+++ src/App.js
@@ -15,16 +15,6 @@ import AdminStaffPage from './pages/AdminPage/AdminStaffPage'
 import AdminDashboardPage from "./pages/AdminPage/AdminDashboardPage"
 // import ....Page from ....
 
-import DongyuPage from './pages/zpractice/Dongyu/Dongyu'
-import KangkangPage from './pages/zpractice/Kangkang/Kangkang'
-import YanboPage from './pages/zpractice/Yanbo/Yanbo'
-import WystanPage from './pages/zpractice/Wystan/Wystan'
-import KathyPage from './pages/zpractice/Kathy/Kathy'
-import ErikPage from './pages/zpractice/Erik/Erik'
-import OliviaPage from './pages/zpractice/Olivia/Olivia'
-import MengxuanPage from './pages/zpractice/Mengxuan/Mengxuan'
-import YanboForm from './pages/zpractice/Yanbo/YanboForm'
-
 function App() {
   // 使用theme方法：
   // https://kitson-broadhurst.medium.com/quickly-set-up-a-theme
@@ -46,15 +36,6 @@ function App() {
 
         <Route path="/ui" exact component={UI} />
 
-        <Route path="/dongyu" exact component={DongyuPage} />
-        <Route path="/Kangkang" exact component={KangkangPage} />
-        <Route path="/Yanbo" exact component={YanboPage} />
-        <Route path="/Yanbo/form" exact component={YanboForm} />
-        <Route path="/wystan" exact component={WystanPage} />
-        <Route path="/kathy" exact component={KathyPage} />
-        <Route path="/erik" exact component={ErikPage} />
-        <Route path="/mengxuan" exact component={MengxuanPage} />
-        <Route path="/Olivia" exact component={OliviaPage} />
         <Route component={ErrorPage} />
       </Switch>
     </ThemeProvider>
